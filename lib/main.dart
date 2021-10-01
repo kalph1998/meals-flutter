a90e820ee5dedb4196f41a7848cef52d38324f6f
kalph <kalph.u@techroversolutions.com>
2021-10-01 17:38:39 +0530

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:meals/categories_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  MyApp({Key? key}) : super(key: key);
+  final ThemeData theme = ThemeData(
+    fontFamily: 'Raleway',
+    textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyText2: const TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          headline6: const TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold),
+        ),
+  );
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: theme.copyWith(
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
       ),
       home: const CategoriesScreen(),
     );
